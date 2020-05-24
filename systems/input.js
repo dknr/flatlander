3bee964760df40ceb66e71decd00f73affd872e9
David Knauer <dknr@dknr.io>
2020-05-24 16:20:17 -0700

    use mouse for input

--- systems/input.js
+++ systems/input.js
@@ -1,33 +1,36 @@
-const keymap = {
-	"w": () => ({ intent: [0,-1] }),
-	"a": () => ({ intent: [-1,0] }),
-	"s": () => ({ intent: [0,1] }),
-	"d": () => ({ intent: [1,0] }),
-}
-const applyEvent = (e, ev) => {
-	const action = keymap[ev.key];
-	if (action === undefined)
-		return e;
-
-	const delta = action(e);
-	return {
-		...e,
-		...delta,
-	}
+const getDistance = (v) =>
+	Math.sqrt(Math.pow(v[0],2) + Math.pow(v[1],2));
+const scaleVector = (v,x) =>
+	[x*v[0],x*v[1]];
+const makeVector = (a,b) =>
+	[b[0]-a[0],b[1]-a[1]];
+const getPath = (a,b,s) => {
+	const v = makeVector(a,b);
+	const d = getDistance(v);
+	return scaleVector(v,d < 1 ? 1 : 1/d);
 }
 export const input = () => {
-	let lastEvent;
-	document.addEventListener("keydown", ev => lastEvent = ev);
-	return (s,t) => {
-		if (lastEvent === undefined)
-			return s;
-		const result = [
-			...s.filter(e => e.input === undefined),
-			...s.filter(e => e.input).map(e => applyEvent(e, lastEvent))
+	let mouse;
+	const canvas = document.getElementById("canvas");
+	canvas.addEventListener("mousemove", ev => {
+		mouse = [
+			(ev.layerX - canvas.offsetLeft) / 10,
+			(ev.layerY - canvas.offsetTop)  / 10,
 		];
-
-		lastEvent = undefined;
-		return result;
+	});
+	return (s,t) => {
+		if (mouse === undefined)
+			return s;
+		return [
+			...s.filter(e => e.input === undefined),
+			...s.filter(e => e.input).map(e => ({
+				...e,
+				intent: getPath(
+					e.material.position,
+					mouse,
+				),
+			})),
+		];
 	}
 }
 
